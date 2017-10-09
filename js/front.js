be997bcbf1700743518343e7775163493d21feca
Nick Hoats <nhoatz@gmail.com>
2017-10-09 13:27:20 -0400

    debugging

--- js/front.js
+++ js/front.js
@@ -4,13 +4,18 @@ $("#contactForm").submit(function(event){
 
 function submitForm(){
     // Initiate Variables With Form Content
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
+    var firstName = $("#form_name").val();
+    var lastName = $("#form_lastname").val();
+    var email = $("#form_email").val();
+    var phone = $("#form_phone").val();
+    var message = $("#form_message").val();
 
     //contact form verification
-    if (!name.value || !email.value || !message.value) {
-      alertify.error("Please make sure Form is completed");
+    if (!form_name.value || !form_lastname.value || !form_email.value ||
+      !form_phone.value || !form_message.value) {
+        
+        alertify.error("Please make sure Form is completed");
+
     } else {
       $.ajax({
         url: "https://formspree.io/you@email.com",
